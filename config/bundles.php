f8810ad5c5956819118162a78a565b6ce63a2c73
jramirez <josepichu@gmail.com>
2018-11-17 17:37:35 +0000

    added security component and added users table

--- config/bundles.php
+++ config/bundles.php
@@ -5,4 +5,5 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
