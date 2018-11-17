db047c1896be5ada2cc6721a1c78b7b264615cf2
jramirez <josepichu@gmail.com>
2018-11-17 19:35:30 +0000

    añadido servicio reservas

--- src/Controller/Reservas/ReservasController.php
+++ src/Controller/Reservas/ReservasController.php
@@ -3,13 +3,15 @@ namespace App\Controller\Reservas;
 
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Controller\ApiController;
+use App\Manager\ReservaManager;
 
-class ReservasController
+class ReservasController extends ApiController
 {
     /**
      * @Route("/disponibilidad", name="reservas.disponibilidad")
      */
-    public function disponibilidad()
+    public function disponibilidad(ReservaManager $reservaManager)
     {
       
         return new JsonResponse([
